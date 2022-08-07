8f170f96d2ab32c8b9d5e73937bb9b87f8ec4cb5
Tom Gustafsson <tom.gustafsson@iki.fi>
2022-08-07 21:59:35 +0300

    Some back and forths

--- configuration.nix
+++ configuration.nix
@@ -5,7 +5,7 @@
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.extraModulePackages = [ config.boot.kernelPackages.v4l2loopback ];
+  # boot.extraModulePackages = [ config.boot.kernelPackages.v4l2loopback ];
 
   networking.hostName = "tunkki";
   networking.networkmanager.enable = true;
@@ -15,7 +15,7 @@
   networking.nameservers = [ "8.8.8.8" "8.8.4.4" ];
 
   # networking.firewall.allowedTCPPortRanges = [
-  #   { from = 8080; to = 8081; }
+  #   { from = 3389; to = 3390; }
   # ];
   # networking.firewall.allowedUDPPortRanges = [
   #   { from = 5000; to = 5003; }
@@ -34,6 +34,13 @@
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
 
+  services.openssh = {
+    enable = true;
+    passwordAuthentication = false;
+    kbdInteractiveAuthentication = false;
+    ports = [ 20022 ];
+  };
+
   # services.printing.enable = true;
   # services.printing.drivers = [ pkgs.gutenprint ];
   
@@ -41,11 +48,14 @@
     isNormalUser = true;
     home = "/home/tom";
     extraGroups = [ "wheel" "networkmanager" "docker" "vboxusers" ];
+    openssh.authorizedKeys.keys = [
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDP7ayrJK6zml2w7g5Xenk/BvrrjrcouNMeX+I2bbICrNqlN8znry5o/aesn0l2shAk0fibe85HPF+WPur+SSf1BauLDPo3rchTADi3vO/ZZUt3NSsdaE3D2x66jucv+aD9lkp+mrzvikREC0zRB6aSErGCUJ7qJU03T8Ckt7C5a3yhWMlu8znJWBS+X6UijmVGYniWtdriBtastEy4mYYv+Rc8ChasxWckVm1JQCjqnh6uqd9NiLoSh7PJe0X62jHbqCUcrfR+8AQsdDUm9kcl65193QC1HkoDGVwL8vqlDUZbevzgvezVE2tVs+hIj3765YmC2A7/1wSIL+tavw3lMVKnqulBLwdrC+meKEYxTYdLvIHDafkWpv6Oyp8jMfwm2sUgoviNwYEWhRoGaSTVp22jyT6UKBbSoYP2x22mThVh96Icj1qmCws5a4ZZ7jxiCoFxv+x27zuQGVQ1AlsdBzuwrWQ++tIa62jPwfXndTVmk4uHGhc2EDVkS9OHW/s="
+    ];
   };
 
   virtualisation.docker.enable = true;
   virtualisation.virtualbox.host.enable = true;
-  virtualisation.virtualbox.host.enableExtensionPack = true;
+  # virtualisation.virtualbox.host.enableExtensionPack = true;
 
   system.stateVersion = "20.09";
 
